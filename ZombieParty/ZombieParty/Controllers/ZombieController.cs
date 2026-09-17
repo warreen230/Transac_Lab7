387fb109cdf16d25b0e94ca174a6b00d9882f5ce
Warren Evans Parmasse <2542540@cegepmontpetit.ca>
2026-09-17 13:58:27 -0400

    : FCT Zombie Delete
    : Ajout de l’action Delete dans ZombieController

--- ZombieParty/ZombieParty/Controllers/ZombieController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -95,5 +95,30 @@ namespace ZombieParty.Controllers
 
             return View(zombieVM);
         }
+
+        public IActionResult Delete(int id)
+        {
+            ZombieVM zombieVM = new ZombieVM();
+            zombieVM.Zombie = _baseDonnees.Zombies.Find(id);
+          
+
+            return View(zombieVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            Zombie? zombie = _baseDonnees.Zombies.Find(id);
+            if (zombie == null)
+            {
+                return NotFound();
+            }
+
+            _baseDonnees.Zombies.Remove(zombie);
+            _baseDonnees.SaveChanges();
+            TempData["Success"] = $"Zombie {zombie.Name} terminated";
+            return RedirectToAction("Index");
+        }
+
     }
 }
